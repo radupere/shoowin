09670423a175dfe6af06ffb471e02c03e84d4639
radu.pereteanu <radu@sickmedia.co>
2018-05-08 15:00:43 +0300

    shoowin

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,38 @@
 $(document).ready(function(){
-	
-	
-	
-	
+
+
+
+
 	/*---------------------- FAQ ----------------------------*/
 	$('.faqQuestion').on('click',function(){
 		$('.faqAnswer').hide();
 		$('#faqAnswer_' + $(this).attr('id').split('_')[1]).toggle();
 	});
-	
-	
-	
+
+
+
 	$('#headerButton').on('click',function(){
 		$('html').animate({ scrollTop: $('.sectionLogos').offset().top }, 700);
 	});
 	$('#headerHowItWorks').on('click',function(){
 		$('html').animate({ scrollTop: $('.sectionHowItWorks').offset().top }, 700);
 	});
-	
+
+
+
+	/*----------------------- LOGIN REGISTER -------------------*/
+	$('.loginTabButton').on('click', function(){
+		$('.loginTabButton').removeClass('loginTabActive');
+		$(this).addClass('loginTabActive');
+
+
+		$('.loginRegisterTab').removeClass('loginRegisterTabActive');
+
+		if( $(this).attr('id').split('_')[1] == 'login' ) {
+			$('#loginTab').addClass('loginRegisterTabActive');
+		} else {
+			$('#registerTab').addClass('loginRegisterTabActive');
+		}
+	});
+
 }); // document ready
